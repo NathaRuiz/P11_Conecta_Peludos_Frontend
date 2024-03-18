fb0bc575bc6902e8f3c9bd33be9be7c3780673f0
NathaRuiz <nruizm98@gmail.com>
2024-03-18 13:55:01 +0100

    create structure of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Router from "./config/Router";
+
 
 function App() {
  
   return (
-    <>
-      <h1 className="text-3xl text-center font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <Router/>
+      </div>
+  
+    </div>
   );
 }
 
